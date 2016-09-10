37371feabbd1bb68d8955e1d768ef69d285fbb29
Allain Lalonde <allain.lalonde@gmail.com>
2016-09-09 22:49:11 -0400

    Adding support for simple static html spec

--- test/verso.js
+++ test/verso.js
@@ -1,5 +1,6 @@
-const test = require('blue-tape')
-const verso = require('..')
+var test = require('blue-tape')
+
+var verso = require('..')
 
 test('setup must have at least 1 page', function (t) {
   try {
@@ -23,95 +24,89 @@ test('pages must be keyed by valud uris', function (t) {
 })
 
 test('render rejects when uri does not match', function (t) {
-  var p = verso({
+  return verso({
     '/': {}
-  })
-
-  return p.render('/missing').then(t.fail, function (err) {
+  }).render('/missing').then(t.fail, function (err) {
     t.equal(err.message, 'missing page: /missing')
   })
 })
 
 test('render generates html', function (t) {
-  var p = verso({
+  return verso({
     '/': {
       render: function () {
         return '<!DOCTYPE html><html><h1>Testing</h1></html>'
       }
     }
-  })
-
-  return p.render('/').then(function (html) {
+  }).render('/').then(function (html) {
     t.equal(html, '<!DOCTYPE html><html><h1>Testing</h1></html>')
   })
 })
 
 test('passes params to render', function (t) {
-  var p = verso({
+  return verso({
     '/a/:a/': {
       render: function (a) {
         return '<!DOCTYPE html><html><h1>' + a + '</h1></html>'
       }
     }
-  })
-
-  return p.render('/a/10').then(function (html) {
+  }).render('/a/10').then(function (html) {
     t.equal(html, '<!DOCTYPE html><html><h1>10</h1></html>')
   })
 })
 
 test('render uses context passed into verso factory', function (t) {
-  var p = verso({
+  return verso({
     '/a': {
       render: function (b) {
         return b
       }
     }
-  }, {b: 'hello'})
-
-  return p.render('/a').then(function (html) {
+  }, {b: 'hello'}).render('/a').then(function (html) {
     t.equal('hello', html)
   })
 })
 
 test('run updates the dom', function (t) {
-  var p = verso({
+  return verso({
     '/test': {
       render: function () {
         return '<div>Testing</div>'
       }
     }
-  })
-
-  return p.run('/test', {})
+  }).run('/test', {})
     .then(function (el) {
       t.equal(el.innerHTML, '<div>Testing</div>')
     })
 })
 
 test('run updates the dom and calls customize on it if its defined', function (t) {
-  var p = verso({
+  return verso({
     '/test': {
       render: function () { return '<div>Testing</div>' },
       customize: function (el) {
         el.innerHTML = el.innerHTML.toUpperCase()
       }
     }
-  })
-
-  return p.run('/test', {}).then(function (el) {
+  }).run('/test', {}).then(function (el) {
     t.equal(el.innerHTML, '<DIV>TESTING</DIV>')
   })
 })
 
 test('page spec may be just be a function', function (t) {
-  var p = verso({
+  return verso({
     '/test': function () {
       return 'testing'
     }
+  }).render('/test').then(function (html) {
+    t.equal(html, 'testing')
   })
+})
 
-  return p.render('/test').then(function (html) {
+test('render may just be a string', function (t) {
+  return verso({
+    '/test': 'testing'
+  }).render('/test').then(function (html) {
     t.equal(html, 'testing')
   })
 })
